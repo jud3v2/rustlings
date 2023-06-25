2179bd128abe9027525f649f71e47f326be1824d
jud3v2 <judikaelbellance@icloud.com>
2023-06-25 14:13:41 +0200

    Fix various Rust concepts, including move semantics, primitive types, and vectors, 30% of rustlings.

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,15 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // this will borrow references of variable A 
+    // and take the range of values between 1 of 4.
+    let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
